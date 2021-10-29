318ec487ffb004687194c2ed5dcb8bb5a813155c
Jeffrey Stewart <jstewartofficial20111@gmail.com>
2021-10-29 12:05:39 -0500

    Debugged server to properly start

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 // eslint-disable-next-line import/extensions
-import authorization from './.config.js';
 
-const path = require("path")
-const express = require("express"); // npm installed
+const path = require('path');
+const express = require('express'); // npm installed
+const authorization = require('./.config');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.use(express.json());
 // other configuration...
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
 
 // TODO: take in any request from the client and append an authorization object
 // with a value of our authorization string to it
