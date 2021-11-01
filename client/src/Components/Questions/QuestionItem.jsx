a8be45086781c0c7a8c94d9363adeebe3953ac82
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-01 12:59:44 -0700

    Added QuestionItem component

---
+++ client/src/Components/Questions/QuestionItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AnswersList from './AnswersList';
+
+const QuestionItem = (props) => {
+  const { question } = props;
+  const { question_body, question_helpfulness, answers } = question;
+  return (
+    <div className="questions-list-item">
+      <section className="questions-list-item-questionbody">
+        <span>Q:</span>
+        <p>{question_body}</p>
+      </section>
+      <aside>
+        <span>Helpful?</span>
+        <button type="button">Yes</button>
+        {question_helpfulness}
+        <button type="button">Add Answer</button>
+      </aside>
+      <section>
+        <span>A:</span>
+        <ul>
+          <AnswersList answerlist={answers} />
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+QuestionItem.propTypes = {
+  question: PropTypes.shape({
+    question_id: PropTypes.number,
+    question_body: PropTypes.string.isRequired,
+    question_date: PropTypes.string.isRequired,
+    asker_name: PropTypes.string.isRequired,
+    question_helpfulness: PropTypes.number.isRequired,
+    reported: PropTypes.bool,
+    answers: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }).isRequired,
+};
+
+export default QuestionItem;
