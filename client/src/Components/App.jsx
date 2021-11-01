b25db0140c88ee6d2b4b9e106e1dcc57c226ab0a
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-01 16:10:38 -0700

    added QuestionAndAnswer to App

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Overview from './Overview/Overview';
 import Reviews from './Review/Reviews';
+import QuestionAndAnswer from './Questions/QuestionAndAnswer';
 
 // please add your components here in the order they appear
 const App = (props) => (<div>
   <Overview/>
   <Reviews/>
   </div>);
+>>>>>>> main
 
 export default App;
