000c0fb5c4e0c16f00aab9574622237f2ad0c27f
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-01 18:43:11 -0700

    Added Q&A sections

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,7 +3,7 @@ import Reviews from './Review/Reviews';
 import QuestionAndAnswer from './Questions/QuestionAndAnswer';
 
 // please add your components here in the order they appear
-const App = (props) => (
+const App = () => (
   <>
     <div><QuestionAndAnswer /></div>
     <div><Reviews /></div>
