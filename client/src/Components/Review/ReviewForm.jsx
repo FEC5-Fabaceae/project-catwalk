4d8d2f54688e69367d0eca44466b326f4c7ad504
April Granzow <apriljgranzow@gmail.com>
2021-11-02 11:01:31 -0700

    MVP Review Submission form HTML

---
+++ client/src/Components/Review/ReviewForm.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const ReviewForm = (props) => (
+  <form>
+    <h1>Submit a review</h1>
+    Fields with an asterisk (*) are required.
+    <br />
+    <fieldset>
+      <legend>Score *</legend>
+      <p>
+        <input name="score" type="radio" value="1"/>
+        <label for="1">1</label>
+        <input name="score" type="radio" value="2" />
+        <label for="2">2</label>
+        <input name="score" type="radio" value="3" />
+        <label for="3">3</label>
+        <input name="score" type="radio" value="4" />
+        <label for="4">4</label>
+        <input name="score" type="radio" value="5" />
+        <label for="5">5</label>
+      </p>
+    </fieldset>
+    <fieldset>
+      <legend>Do you recommend this product?</legend>
+      <p>
+        <h3><label for="recommend"></label></h3>
+        <input name="recommend" type="radio" id="yes" value="yes" />
+        <label for="yes">Yes</label>
+        <input name="recommend" type="radio" id="no" />
+        <label for="no">No</label>
+      </p>
+    </fieldset>
+    <fieldset>
+      <legend>Characteristics</legend>
+    </fieldset>
+    {/* We need to decide which descriptors are relevant to the given item */}
+    <fieldset>
+      <legend>Review Body</legend>
+      <p>
+      <h3><label for="summary">Summary</label></h3>
+      <input type="text" id="summary" placeholder="Example: Best purchase ever!" />
+      </p>
+        <h3><label for="body">Body*</label></h3>
+        <textarea>Why did you like the product or not?</textarea>
+      <p>Minimum required characters left: 50</p>
+    </fieldset>
+    <fieldset>
+      <legend>Upload Photos</legend>
+      <input type="file" />
+    </fieldset>
+    <fieldset>
+      <legend>Name and Email*</legend>
+      <h3><label for="name">Your name*</label></h3>
+    <input type="text" placeholder="Example: jackson11" />
+    <h3><label for="email">Your email*</label></h3>
+    <input type="text" placeholder="Example: jackson11@email.com" />
+    <p>For authentication reasons, you will not be emailed.</p>
+    </fieldset>
+    <button type="submit">Submit</button>
+  </form>
+);
