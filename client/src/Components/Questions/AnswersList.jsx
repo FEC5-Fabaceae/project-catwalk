d80d2cb50267635bd3cab3b0bcbc62f381d2ac52
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-03 14:58:04 -0700

    added button

--- client/src/Components/Questions/AnswersList.jsx
+++ client/src/Components/Questions/AnswersList.jsx
@@ -1,14 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AnswerItem from './AnswerItem';
 
 const AnswersList = (props) => {
   const { answerlist, productID, setQuestions } = props;
   const answers = Object.values(answerlist);
+  const [visible, setVisible] = useState(2);
+
+  const clickButton = (e) => {
+    if (e.target.value === 'more') {
+      setVisible(answers.length);
+    }
+    if (e.target.value === 'collapse') {
+      setVisible(2);
+    }
+  };
+
+  let AddCollapseButton;
+  if (answers.length > 2 && answers.length !== visible.length) {
+    AddCollapseButton = <button type="button" value="more" onClick={(e) => { clickButton(e); }}>See more answers</button>;
+  }
+  if (answers.length === visible.length && answers.length !== 2) {
+    AddCollapseButton = <button type="button" value="collapse" onClick={(e) => { clickButton(e); }}>Collapse answers</button>;
+  }
 
   return (
     <div>
-      <ul>
+      <ul className="answer-list">
         {answers.map(
           (answer) => (
             <AnswerItem
@@ -20,6 +38,7 @@ const AnswersList = (props) => {
           ),
         )}
       </ul>
+      <div className="answer-list-button">{AddCollapseButton}</div>
     </div>
   );
 };
