8ca11a5662961864dd97f790b3358cb5c6e9195c
April Granzow <apriljgranzow@gmail.com>
2021-11-02 20:28:46 -0700

    Created context object and refactored App to be a Context Provider

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Overview from './Overview/Overview';
 import Reviews from './Review/Reviews';
 import QuestionAndAnswer from './Questions/QuestionAndAnswer';
+import ProductIdContext from './Context';
 
 // please add your components here in the order they appear
-
 const App = (props) => (
   <>
-    <div><Overview /></div>
-    <div><QuestionAndAnswer /></div>
-    <div><Reviews /></div>
+    <ProductIdContext.Provider value="40345">
+      console.log(ProductIdContext);
+      <div><Overview /></div>
+      <div><QuestionAndAnswer /></div>
+      <div><Reviews /></div>
+    </ProductIdContext.Provider>
   </>
 );
 
---
+++ client/src/Components/Context.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ProductIdContext = React.createContext('40344');
+
+export default ProductIdContext;
