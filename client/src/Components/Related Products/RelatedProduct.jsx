45a85a8f09a2ef43d09c87941773cf0d915be5a9
Jeffrey Stewart <jstewartofficial20111@gmail.com>
2021-11-03 17:34:19 -0500

    Added all elements of a related product

--- client/src/Components/Related Products/RelatedProduct.jsx
+++ client/src/Components/Related Products/RelatedProduct.jsx
@@ -17,50 +17,52 @@ const RelatedProduct = (props) => {
         const { name } = relatedProduct;
         const { category } = relatedProduct;
         setProduct({ name, category });
-      })
+        axios.get(`http://localhost:3000/products/${product}/styles`)
+          .then((data) => {
+            const styles = data.data.results;
+            let defaultStyle = styles[0];
+            for (let i = 0; i < styles.length; i += 1) {
+              if (styles[i]['default?'] === true) {
+                defaultStyle = styles[i];
+              }
+            }
+            const photo = defaultStyle.photos[0].thumbnail_url;
+            let salePrice = defaultStyle.original_price;
+            if (defaultStyle.sale_price !== null) {
+              setSaleStatus(true);
+              salePrice = defaultStyle.sale_price;
+            }
+            setStylesInfo({
+              photo,
+              salePrice,
+            });
+            axios({
+              method: 'get',
+              url: 'http://localhost:3000/reviews/meta',
+              params: {
+                product_id: product,
+              },
+            })
+              .then((data) => {
+                const rating = data.data.ratings;
+                let totalRatings = 0;
+                let totalValues = 0;
+                let index = 1;
+                for (let key in rating) {
+                  totalRatings = totalRatings + parseInt(rating[key], 10);
+                }
+                for (let key in rating) {
+                  totalValues = totalValues + parseInt(rating[key], 10) * parseInt(key, 10);
+                }
+                const average = (totalValues / totalRatings);
+                setRatings(average);
+              });
+          });
+      });
     // get the product styles to find the default style and its associated price.
     // also contains the photo we need
-    axios.get(`http://localhost:3000/products/${product}/styles`)
-      .then((data) => {
-        const styles = data.data.results;
-        let defaultStyle = styles[0];
-        for (let i = 0; i < styles.length; i += 1) {
-          if (styles[i]['default?'] === true) {
-            defaultStyle = styles[i];
-          }
-        }
-        const photo = defaultStyle.photos[0].thumbnail_url;
-        let salePrice = defaultStyle.original_price;
-        if (defaultStyle.sale_price !== null) {
-          setSaleStatus(true);
-          salePrice = defaultStyle.sale_price;
-        }
-        setStylesInfo({
-          photo,
-          salePrice,
-        });
-      });
     // get the review metadata and calculate average
-    axios({
-      method: 'get',
-      url: 'http://localhost:3000/reviews/meta',
-      params: {
-        product_id: product,
-      },
-    })
-      .then((data) => {
-        console.log(data.data);
-        console.log(data.data.ratings);
-        const rating = data.data.ratings;
-        const totalRatings = (rating['1'] + rating['2'] + rating['3'] + rating['4'] + rating['5']);
-        const totalValues = ((rating['1'] * 1) + (rating['2'] * 2) + (rating['3'] * 3) + (rating['4'] * 4) + (rating['5'] * 5));
-        const average = (totalValues / totalRatings);
-        console.log(totalValues);
-        console.log(totalRatings);
-        console.log(average);
-        setRatings(average);
-      });
-  }, [relatedProductInfo]);
+  }, [product]);
   return (
 
     <div>
