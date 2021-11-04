049034df43796d7a1d3db18889824460756646bd
Makeda Davis <1makedadavis@gmail.com>
2021-11-04 08:10:45 -0400

    refactured overview, added styles.css, created gallery and product info components

---
+++ client/src/Components/Overview/Gallery.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Gallery = () => (
+<div>GALLERY</div>
+)
+
+
+export default Gallery;
--- client/src/Components/Overview/Overview.jsx
+++ client/src/Components/Overview/Overview.jsx
@@ -1,37 +1,24 @@
 import React, { useState } from 'react';
+import Gallery from './Gallery';
+import ProductInfo from './ProductInfo';
 
 const Overview = () => (
+  // const []
   <div>
     <div>
       <p>
         <em>SITE-WIDE ANNOUNCEMENT MESSAGE! </em>
         —— SALE/DISCOUNT
-        <b>OFFER</b>
+        <b> OFFER</b>
         ——
         {/* add href  */}
         <a href>NEW PRODUCT HIGHLIGHT</a>
       </p>
       <div className="left">
-        gallery
+        <Gallery />
       </div>
       <div className="right">
-        <div className="star"> Star</div>
-        <a href>Read all reviews </a>
-        {/* add href */}
-        <p>CATEGORY</p>
-        <h1>Expanded Product Name</h1>
-        <p>price</p>
-        {/* style selector */}
-        <div>
-          <b>
-            STYLE
-            {'>'}
-          </b>
-          SELECTED STYLE
-        </div>
-        <div>thumbnails</div>
-        <div>select size and quantity</div>
-        <div>add to cart and</div>
+        <ProductInfo />
       </div>
     </div>
   </div>
