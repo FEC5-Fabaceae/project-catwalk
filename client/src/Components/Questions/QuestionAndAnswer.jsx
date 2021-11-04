e8ec4713ac3bb1c0e6ea6d12fff4d8f0283ccfa5
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-04 11:08:26 -0700

    added search bar

--- client/src/Components/Questions/QuestionAndAnswer.jsx
+++ client/src/Components/Questions/QuestionAndAnswer.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import SearchBar from './SearchBar';
 import QuestionsList from './QuestionsList';
 
 const QuestionAndAnswer = () => {
   const [questionsState, setQuestionsState] = useState({});
 
   const retrieveQuestions = () => {
-    // for now, change ${product_id} to 40350
-    axios.get('/qa/questions/?product_id=40050')
+    // for now, change ${product_id} to 40345
+    axios.get('/qa/questions/?product_id=40345')
       .then((res) => {
         setQuestionsState(res.data);
       })
@@ -21,7 +22,9 @@ const QuestionAndAnswer = () => {
   return (
     <section className="questions-answers-section">
       <h2>Questions and Answers</h2>
-      <div className="questions-search" />
+      <div className="questions-search">
+        <SearchBar list={questionsState.results} changelist={setQuestionsState} />
+      </div>
       <div className="questions-main">
         <QuestionsList list={questionsState} />
       </div>
