2d761025d61e071de20f32e433d533a6d253ce2e
Makeda Davis <1makedadavis@gmail.com>
2021-11-04 09:37:05 -0400

    rendered image from api

--- client/src/Components/Overview/Gallery.jsx
+++ client/src/Components/Overview/Gallery.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-const Gallery = () => (
-<div>GALLERY</div>
-)
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const Gallery = () => {
+  const [productURL, setProductUrl] = useState('');
+  const getProduct = () => {
+    axios.get('/products/40350/styles')
+      .then((res) => {
+        console.log(res.data.results[0].photos[0].url);
+        setProductUrl(res.data.results[0].photos[0].url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  getProduct();
+  return (
+    <div className="left-column">
+      <img src={productURL} alt="clothing" />
+    </div>
+  );
+};
 
 export default Gallery;
