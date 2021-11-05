6b6ba65d56ee842e8d97ee27a0c1cd2c69b00610
Jeffrey Stewart <jstewartofficial20111@gmail.com>
2021-11-05 15:16:20 -0500

    Added basic outfit list component

---
+++ client/src/Components/Related Products/AddOutfit.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const axios = require('axios');
+
+const AddOutfit = (props) => {
+  const { product } = props;
+  const { arrayOfOutfits } = props;
+  const { setArray } = props;
+  const updateArray = () => {
+    let isFound = false;
+    for (let i = 0; i < arrayOfOutfits.length; i += 1) {
+      if (product === arrayOfOutfits[i]) {
+        isFound = true;
+      }
+    }
+    if (!isFound) {
+      setArray(() => [...arrayOfOutfits, product]);
+    }
+  };
+
+  return (
+    <li className="carousel-card">
+      <i className="far fa-plus-square" type="button" onClick={(e) => {updateArray()}} />
+      <p>Add Current Product To Outfit</p>
+    </li>
+  );
+};
+
+export default AddOutfit;
--- client/src/Components/Related Products/OutfitList.jsx
+++ client/src/Components/Related Products/OutfitList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
-import RelatedProduct from './Outfit';
+import Outfit from './Outfit';
+import AddOutfit from './AddOutfit';
 import ProductIdContext from '../Context';
 
 const axios = require('axios');
@@ -7,69 +8,102 @@ const axios = require('axios');
 // send a get request for each related product from the given product id
 // render the default style for each related product
 
-const RelatedProductsList = (props) => {
+const OutfitProductsList = (props) => {
   const [leftVisible, setLeftVisible] = useState('hidden');
   const [rightVisible, setRightVisible] = useState('visible');
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(3);
   const value = useContext(ProductIdContext);
   const [productID, setProductID] = useState(value);
-  const [arrayOfRelatedProducts, setArray] = useState([]);
-  const [visibleProducts, setVisible] = useState([arrayOfRelatedProducts.slice(0, 2)]);
+  const [arrayOfOutfits, setArray] = useState([]);
+  const [visibleProducts, setVisible] = useState([arrayOfOutfits.slice(0, 2)]);
   useEffect(() => {
-    axios.get(`http://localhost:3000/products/${productID}/related`)
-      .then((data) => {
-        setArray(data.data);
-        setVisible(data.data.slice(start, end));
-      });
-  }, [productID]);
+    setVisible(arrayOfOutfits.slice(start, end));
+  }, [arrayOfOutfits]);
   const previousSlide = (e) => {
-    if (end === arrayOfRelatedProducts.length) {
+    if (end === arrayOfOutfits.length) {
       setRightVisible('visible');
     }
-    if (start - 1 === 0) {
+    if (start - 1 === 0 || arrayOfOutfits.length === 0) {
       setLeftVisible('hidden');
     }
     if (start > 0) {
       setStart(start - 1);
       setEnd(end - 1);
-      setVisible(arrayOfRelatedProducts.slice(start - 1, end - 1));
+      setVisible(arrayOfOutfits.slice(start - 1, end - 1));
     }
   };
   const nextSlide = (e) => {
-    if (start === 0) {
+    console.log(arrayOfOutfits);
+    if (start === 0 && arrayOfOutfits.length > 3) {
       setLeftVisible('visible');
     }
-    if (end + 1 === arrayOfRelatedProducts.length) {
+    if (end + 1 >= arrayOfOutfits.length) {
       setRightVisible('hidden');
     }
-    if (end < arrayOfRelatedProducts.length) {
+    if (end < arrayOfOutfits.length) {
       setStart(start + 1);
       setEnd(end + 1);
-      setVisible(arrayOfRelatedProducts.slice(start + 1, end + 1));
+      setVisible(arrayOfOutfits.slice(start + 1, end + 1));
     }
   };
+  if (arrayOfOutfits.length > 3) {
+    return (
+      <div className="carousel-row">
+        <h2 className="carousel-title"> Your Outfit </h2>
+        <div className="carousel-inner-content">
+          <div className="carousel-col carousel-left">
+            <i className="fas fa-step-backward" role="button" onClick={previousSlide} style={{ visibility: leftVisible }} />
+          </div>
+          <div className="carousel-col carousel-center">
+            <div className="carousel-viewport">
+              <ol className="carousel">
+                <AddOutfit product={productID} arrayOfOutfits={arrayOfOutfits} setArray={setArray} />
+                {visibleProducts.map((outfitProductID) => (
+                  <Outfit product={outfitProductID} />
+                ))}
+              </ol>
+            </div>
+          </div>
+          <div className="carousel-col carousel-goto-right">
+            <i className="fas fa-step-forward" role="button" onClick={nextSlide} style={{ visibility: rightVisible }} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  if (arrayOfOutfits.length > 0) {
+    return (
+      <div className="carousel-row">
+        <h2 className="carousel-title"> Your Outfit </h2>
+        <div className="carousel-inner-content">
+          <div className="carousel-col carousel-center">
+            <div className="carousel-viewport">
+              <ol className="carousel">
+                <AddOutfit product={productID} arrayOfOutfits={arrayOfOutfits} setArray={setArray} />
+                {visibleProducts.map((outfitProductID) => (
+                  <Outfit product={outfitProductID} />
+                ))}
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="carousel-row">
-      <h2 className="carousel-title"> Related Products </h2>
+      <h2 className="carousel-title"> Your Outfit </h2>
       <div className="carousel-inner-content">
-        <div className="carousel-col carousel-left">
-          <i className="fas fa-step-backward" role="button" onClick={previousSlide} style={{ visibility: leftVisible}}/>
-        </div>
         <div className="carousel-col carousel-center">
           <div className="carousel-viewport">
             <ol className="carousel">
-              {visibleProducts.map((relatedProductID) => (
-                <RelatedProduct product={relatedProductID} />
-              ))}
+              <AddOutfit product={productID} arrayOfOutfits={arrayOfOutfits} setArray={setArray} />
             </ol>
           </div>
-        </div>
-        <div className="carousel-col carousel-goto-right">
-          <i className="fas fa-step-forward" role="button" onClick={nextSlide} style={{ visibility: rightVisible }} />
         </div>
       </div>
     </div>
   );
 };
-export default RelatedProductsList;
+export default OutfitProductsList;
