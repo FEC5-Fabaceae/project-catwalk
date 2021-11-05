6012364fd617b98fdce6218e7620b4216e877e5d
April Granzow <apriljgranzow@gmail.com>
2021-11-05 12:16:46 -0700

    Added command to compile Sass

--- spec/Reviews.test.js
+++ spec/Reviews.test.js
@@ -4,6 +4,8 @@ import { unmountComponentAtNode } from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import { toBeInTheDocument } from '@testing-library/jest-dom';
 import Reviews from '../client/src/Components/Review/Reviews';
+import RatingBreakdown, { sumValues, getPercentages } from '../client/src/Components/Review/RatingBreakdown';
+import metadata from '../client/src/Components/Review/sampleMetaData';
 // import ReviewList from '../client/src/Components/Review/ReviewList';
 // import ProductBreakdown from '../client/src/Components/Review/ProductBreakdown';
 // import RatingBreakdown from '../client/src/Components/Review/RatingBreakdown';
@@ -28,3 +30,33 @@ describe('Reviews component', () => {
     expect(screen.getByText('RATINGS AND REVIEWS')).toBeInTheDocument;
   });
 });
+
+describe('Breakdown Section', () => {
+  describe('Ratings Breakdown', () => {
+    const ratingsObject = {
+      1: 20,
+      2: 10,
+      3: 5,
+      4: 100,
+      5: 2,
+    };
+    describe('Value sum function', () => {
+      test('returns the sum of the values of all properties of an object', () => {
+        const sum = 20 + 10 + 5 + 100 + 2;
+        const total = sumValues(ratingsObject);
+        expect(total).toBe(sum);
+      });
+    });
+
+    describe('Percentages calculator', () => {
+      test('creates an object with ratings mapped to their percentage of the total number of ratings', () => {
+        const percentagesObject = getPercentages(ratingsObject);
+        expect(percentagesObject).toBeInstanceOf(Object);
+
+        }
+      )}
+    );
+  }
+};
+
+
