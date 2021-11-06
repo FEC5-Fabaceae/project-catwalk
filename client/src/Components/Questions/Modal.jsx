9b2dfc5f06d6054bf5459d5b68d6cf60c4a5f51f
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-06 12:44:02 -0700

    basic modal

--- client/src/Components/Questions/Modal.jsx
+++ client/src/Components/Questions/Modal.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Modal = (props) => {
-  //
-}
+  const { component } = props;
+
+  return (
+    <div className="modal-container">
+      <div className="modal-component">
+        {component}
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
