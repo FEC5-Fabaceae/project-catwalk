4332db25d2fdd25006c2e0ff67135bc9d0b1c973
April Granzow <apriljgranzow@gmail.com>
2021-11-08 22:23:37 -0800

    Merge

--- client/src/Components/Review/ReviewList.jsx
+++ client/src/Components/Review/ReviewList.jsx
@@ -69,13 +69,14 @@ const ReviewList = () => {
           type="button"
           className="load-more-reviews"
           disabled={loadedAll}
-          onClick={loadMoreReviews}>
+          onClick={loadMoreReviews}
+        >
           Load more reviews
         </button>
         <button
           type="button"
           className="submit-review"
-          >
+        >
           Submit a review
         </button>
       </section>
